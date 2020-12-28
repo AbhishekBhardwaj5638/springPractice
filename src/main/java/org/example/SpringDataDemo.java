4a3cbdc89be9685b3e67c73ceac5695590668778
Abhishek Bhardwaj <abhishekbhardwaj@Abhisheks-MacBook-Air.local>
2020-12-28 00:45:22 -0500

    resolved classnotfound exception

--- src/main/java/org/example/SpringDataDemo.java
+++ src/main/java/org/example/SpringDataDemo.java
@@ -11,22 +11,22 @@ public class SpringDataDemo {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring_configuration.xml");
 
-        //Fetching the DAO from Spring Bean Factory
-//        EmployeeDAO employeeDAO= (EmployeeDAO)context.getBean("EmployeeDaoImpl");
-//        Employee employee = new Employee("employee123");
-//
-//        //employee.setEmployeeId("1");
-//
-//        //Save an employee Object using the configured Data Source
-//        employeeDAO.save(employee);
-//        System.out.println("Employee Saved with EmployeeId"+employee.getEmployeeName());
-//
-//        // find an object using PrimaryKey
-//        Employee emp = employeeDAO.findById(employee.getEmployeeId());
-//        System.out.println(emp);
-//
-//        //Close the ApplicationContext
-//        ((ConfigurableApplicationContext)context).close();
+       // Fetching the DAO from Spring Bean Factory
+        EmployeeDAO employeeDAO= (EmployeeDAO)context.getBean("EmployeeDaoImpl");
+        Employee employee = new Employee("employee123");
+
+        //employee.setEmployeeId("1");
+
+        //Save an employee Object using the configured Data Source
+        employeeDAO.save(employee);
+        System.out.println("Employee Saved with EmployeeId"+employee.getEmployeeName());
+
+        // find an object using PrimaryKey
+        Employee emp = employeeDAO.findById(employee.getEmployeeId());
+        System.out.println(emp);
+
+        //Close the ApplicationContext
+        ((ConfigurableApplicationContext)context).close();
 
     }
 }
